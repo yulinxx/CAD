76340e7ab9fed43294b58ef5c103f13efb48c6a4
xx <xx@win.com>
2026-08-12 20:14:49 +0800

    Win/Mac合并,Win渲染正常

--- Main/Src/main.cpp
+++ Main/Src/main.cpp
@@ -12,13 +12,18 @@ int main(int argc, char** argv)
     // "Could not create NSOpenGLContext with shared context, falling back to
     // unshared context" and the widget renders black.
     // Setting the SAME format as the global default makes the contexts shareable.
+    //
+    // Windows: RenderWidget3D 仍使用固定管线（glBegin/glMatrixMode/glFrustum 等），
+    // 在 CoreProfile 下这些函数会被禁用导致 3D 网格与模型不显示。
+    // 因此 Windows 采用 CompatibilityProfile 兼容现代 shader 与固定管线。
     QSurfaceFormat format;
 #ifdef Q_OS_MACOS
     format.setVersion(4, 1);
+    format.setProfile(QSurfaceFormat::CoreProfile);
 #else
     format.setVersion(4, 6);
+    format.setProfile(QSurfaceFormat::CompatibilityProfile);
 #endif
-    format.setProfile(QSurfaceFormat::CoreProfile);
     format.setDepthBufferSize(24);
     format.setStencilBufferSize(8);
     format.setSamples(0);
